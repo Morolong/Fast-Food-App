c94dc5f77c8bb93be45f59299a0e5972b24ad393
Morolong <mg.morolong@gmail.com>
2026-07-20 16:19:02 +0200

    Add Input Fields for Login and Register Option

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart'; 
+import 'package:hungry_people/components/textfield.dart';
+import 'package:hungry_people/components/my_button.dart';
 
-class LoginPage extends StatelessWidget{
-  const LoginPage({super.key}); 
+class LoginPage extends StatefulWidget{
+  final void Function()? onTap; 
+
+  const LoginPage({super.key, required this.onTap}); 
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>{
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();  
 
   @override
   Widget build(BuildContext context){
@@ -15,10 +28,63 @@ class LoginPage extends StatelessWidget{
               Icons.lock_open_rounded, 
               size: 100, 
               color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 25), 
+
+            Text(
+              "Food Delivery App", 
+              style: TextStyle(
+                fontSize:16, 
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25), 
+
+            MyTextField(
+              controller: emailController, 
+              hintText: "Email", 
+              obscureText: false,
+            ), 
+            const SizedBox(height: 10), 
+
+               MyTextField(
+              controller: passwordController, 
+              hintText: "Password", 
+              obscureText: true,
+            ), 
+            const SizedBox(height: 10),
+
+            MyButton(
+              text: "Sign In", 
+              onTap: () {},
+            ), 
+            const SizedBox(height: 25),
+
+            Row(
+              mainAxisAlignment:MainAxisAlignment.center, 
+              children: [
+                Text(
+                  "Not a member?", 
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                  const SizedBox(width: 4), 
+                  GestureDetector(
+                    onTap: widget.onTap, 
+                    child: Text(
+                      "Register Now", 
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary, 
+                        fontWeight: FontWeight.bold,
+                      )
+                    )
+                  )
+              ]
             )
-          ]
-          )
-      ,)
+          ],
+          ),
+      ),
     ); 
   }
 }
