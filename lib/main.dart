62f62e2d0e6c71db034cce330b16c3a59af16715
Morolong <mg.morolong@gmail.com>
2026-07-21 18:00:16 +0200

    Set home screen to be handled by LoginOrRegister method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart'; 
 import 'package:hungry_people/themes/theme_provider.dart'; 
 import 'package:provider/provider.dart';
+import 'package:hungry_people/auth/login_or_register.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
@@ -17,9 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp( 
       debugShowCheckedModeBanner: false,
-      home: LoginPage(
-        onTap: () {}, 
-      ), 
+      home: const LoginOrRegister(), 
       theme: Provider.of<ThemeProvider>(context).themeData, 
     );     
   }
